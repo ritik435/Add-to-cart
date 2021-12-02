a3ea3f31194050f59ac666582f78993ace948ffc
Ritik <ritik12022001@gmail.com>
2021-12-02 07:43:25 +0530

    set state function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,20 +1,70 @@
-import { render } from "@testing-library/react";
 import React from "react";
-import react from "react";
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: "Mobile Phone",
+            qty: 1,
+            img: ""
+        }
+    }
+    increaseQty = () => {
+        this.setState((prevState) => {
+            return {
+
+                qty: prevState.qty + 1
+            }
+        }, () => {
+            console.log('this.state', this.state);
+
+        })
+    }
+    decreaseQty = () => {
+        this.setState((prevState) => {
+            {
+                if (prevState.qty > 0) {
+                    prevState.qty -= 1
+                }
+            }
+            return {
+                qty: prevState.qty
+            }
+        }, () => {
+            console.log('this.state', this.state);
+        })
+    }
     render() {
+        const { price, title, qty } = this.state;
         return (
             <div className="cart-item" >
                 <div className="left-block" >
                     <img style={styles.image} />
                 </div>
                 <div className="right-block" style={styles.right}>
-                    <div style={{ fontSize: 40 }}>Phone</div>
-                    <div style={{ color: "#777" }}>Rs 999</div>
-                    <div style={{ color: "#777" }}>Qty: 1</div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: "#777" }}>Rs {price}</div>
+                    <div style={{ color: "#777" }}>Qty: {qty}</div>
                     <div className="cart-item-actions">
-                        {/* buttons */}
+                        <img
+                            alt="increase"
+                            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                            className="action-icon"
+                            onClick={this.increaseQty}
+                        />
+                        <img
+                            alt="decrease"
+                            src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+                            className="action-icon"
+                            onClick={this.decreaseQty}
+                        />
+                        <img
+                            alt="delete"
+                            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+                            className="action-icon"
+
+                        />
                     </div>
                 </div>
             </div>
@@ -29,8 +79,6 @@ const styles = {
         backgroundColor: "#ccc"
     },
     right: {
-        height: 110,
-        width: 110,
 
 
     }
